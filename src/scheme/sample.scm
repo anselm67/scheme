ab8bb3d6107fc5a5bc6dbd5ce610be14204c9d4e
anselm <abairdsmith@gmail.com>
2026-02-13 19:51:27 +0100

    Making sure tail recursion works and fails as expected.

--- src/scheme/sample.scm
+++ src/scheme/sample.scm
@@ -1,5 +1,13 @@
 
-(define counter
-    (let ((value 0))
-        (lambda () (set! value (+ value 1)) value))
+(define (tail start end) 
+    (debug start ".." end)
+    (if (< start end) 
+        (tail (+ start 1) end)
+        end))
+
+(define (not-tail count) 
+    (debug count)
+    (not-tail (+ count 1))
+    ;; Not reached!
+    ()
 )
